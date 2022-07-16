b8dc9400ea7e9b3f1679d22024b0b945241ce596
Kirillbiliashov <Kirillbeliashov14@gmail.com>
2022-07-16 13:51:12 +0300

    New class describing single column was added

---
+++ src/main/java/src/Column.java
@@ -0,0 +1,107 @@
+package src;
+
+public class Column {
+  private final String name;
+  private  String type;
+  private final String LINE_SEPARATOR = ", \n";
+  private boolean isNotNull;
+  private boolean isUnique;
+  private boolean isAutoIncrement;
+  private boolean isPrimaryKey;
+  private String defaultValue;
+  private String referenceCol;
+  private String checkStr;
+  private boolean isCascadeDelete;
+
+  public Column(final String name, final String type) {
+    this.name = name;
+    this.type = type;
+  }
+
+  public String toString() {
+    return this.name + " " + this.type + this.getNotNullStr() +
+        this.getAutoIncStr() + this.getDefaultValueStr() +
+        this.getUniqueStr() + this.getPrimaryKeyStr() +
+        this.getForeignKeyStr() + this.getCheckStr();
+  }
+
+  protected void setDataType(final String dataType) {
+    this.type = dataType;
+  }
+
+  protected void setNotNull() {
+    this.isNotNull = true;
+  }
+
+  protected void setUnique() {
+    this.isUnique = true;
+
+  }
+
+  protected void setAutoIncrement() {
+    this.isAutoIncrement = true;
+
+  }
+
+  protected void setDefaultValue(final String defaultValue) {
+    this.defaultValue = defaultValue;
+
+  }
+
+  protected void setPrimaryKey() {
+    this.isPrimaryKey = true;
+
+  }
+
+  protected void setReferenceCol(final String tableName,
+                                 final String foreignField,
+                                 final boolean isCascadeDelete) {
+    this.referenceCol = tableName + "(" + foreignField + ")";
+    this.isCascadeDelete = isCascadeDelete;
+  }
+
+  protected void setCheck(final String operator, final String value) {
+    this.checkStr = operator + value;
+  }
+
+  public boolean isAutoIncrement() {
+    return this.isAutoIncrement;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  private String getNotNullStr() {
+    return this.isNotNull ? " NOT NULL" : "";
+  }
+
+  private String getAutoIncStr() {
+    return this.isAutoIncrement ? " AUTO_INCREMENT" : "";
+  }
+
+  private String getDefaultValueStr() {
+    return defaultValue == null ? "" : " DEFAULT " + defaultValue;
+  }
+
+  private String getPrimaryKeyStr() {
+    return this.isPrimaryKey ? LINE_SEPARATOR + "PRIMARY KEY (" + this.name +
+        ")" : "";
+  }
+
+  private String getUniqueStr() {
+    return isUnique ? LINE_SEPARATOR + "UNIQUE(" + this.name + ")" : "";
+  }
+
+  private String getForeignKeyStr() {
+    final String cascadeDeleteStr = this.isCascadeDelete ? " ON DELETE CASCADE" : "";
+    return this.referenceCol == null ? "" : LINE_SEPARATOR + "FOREIGN KEY " +
+        this.name + " REFERENCES " + this.referenceCol + cascadeDeleteStr;
+  }
+
+  private String getCheckStr() {
+    return this.checkStr == null ? "" : LINE_SEPARATOR + "CHECK (" +
+        this.name + this.checkStr + ")";
+  }
+
+}
--- src/main/java/src/Table.java
+++ src/main/java/src/Table.java
@@ -4,21 +4,16 @@ import queryBuilders.*;
 
 import java.sql.*;
 import java.util.*;
+
 import static src.ConnectionSingleton.*;
+
 public class Table {
 
   private final String tableName;
-  private final ArrayList<String> colNames;
-  private final ArrayList<String> colTypes;
   private final String LINE_SEPARATOR = ", \n";
-  private final List<String> foreignKeyConstraints;
-  private final List<Integer> autoIncColIndices;
-  private final Map<Integer, String> defaultValues;
-  private final List<String> uniqueColStatements;
-  private final List<String> checkStatements;
   private final int colsCount;
-  private String primaryKeyColName;
-  private List<Integer> notNullColIndices;
+  private final List<Column> columnsList;
+  private Column currColumn;
 
   public Table(final String tableName, final String[] colNames,
                final String[] colTypes) throws Exception {
@@ -29,40 +24,28 @@ public class Table {
       throw new Exception(exceptionStr);
     }
     this.tableName = tableName;
-    this.colNames = new ArrayList<>(Arrays.asList(colNames));
-    this.colTypes = new ArrayList<>(Arrays.asList(colTypes));
     this.colsCount = colNames.length;
-    this.foreignKeyConstraints = new ArrayList<>(colNames.length);
-    this.notNullColIndices = new ArrayList<>(colNames.length);
-    this.autoIncColIndices = new ArrayList<>(colNames.length);
-    this.defaultValues = new HashMap<>(colNames.length);
-    this.uniqueColStatements = new ArrayList<>(colNames.length);
-    this.checkStatements = new ArrayList<>(colNames.length);
+    this.columnsList = this.populateColsList(colNames, colTypes);
+  }
+
+  private List<Column> populateColsList(final String[] colNames,
+                                        final String[] colTypes) {
+    final List<Column> columnsList = new ArrayList<>(this.colsCount);
+    for (int i = 0; i < this.colsCount; i++) {
+      columnsList.add(new Column(colNames[i], colTypes[i]));
+    }
+    return columnsList;
   }
 
   public void create() throws SQLException {
     try (final Statement stmt = getConn().createStatement()) {
-      final String sqlStr = this.getCreateTableStatement();
+      final String sqlStr = "CREATE TABLE IF NOT EXISTS " + this.tableName +
+          " (" + this.getColumnsStr() + ")";
       System.out.println(sqlStr);
       if (stmt.execute(sqlStr)) {
         System.out.println("table successfully added to the database");
       }
     }
-  }
-
-  private String getCreateTableStatement() {
-    final String checkStr = this.getConstraintStr(this.checkStatements);
-    final String foreignKeyStr = this.getConstraintStr(this.foreignKeyConstraints);
-    final String uniqueStr = this.getConstraintStr(this.uniqueColStatements);
-    return "CREATE TABLE IF NOT EXISTS " + this.tableName + " (" +
-        this.createColumnsStr() + checkStr + uniqueStr +
-        this.getPrimaryKeyStr() + foreignKeyStr + ");";
-  }
-
-  private String getConstraintStr(final List<String> constraintsList) {
-    final boolean constraintExists = !constraintsList.isEmpty();
-    return constraintExists ? LINE_SEPARATOR +
-        String.join(LINE_SEPARATOR, constraintsList) : "";
   }
 
   public UpdateQueryBuilder update() {
@@ -82,41 +65,11 @@ public class Table {
       }
     }
   }
-  public ColumnController column() {
-    return new ColumnController(this.tableName, this.colNames, this.colTypes);
+
+  public ColumnsController columns() {
+    return new ColumnsController(this.tableName, this.columnsList);
   }
 
-  public void addCheck(final String colName, final String operator,
-                       final String value) throws SQLException {
-    if (this.colNames.contains(colName)) {
-      final String alterStr = "ALTER TABLE " + this.tableName +
-          " ADD CHECK (" + colName + operator + value + ")";
-      this.executeStatement(alterStr);
-    }
-  }
-
-  public void createIndex(final String idxName, final boolean isUnique,
-                          final String... colNames) throws SQLException {
-    final String isUniqueStr = isUnique ? "UNIQUE" : "";
-    final String colNamesStr = String.join(", ", colNames);
-    final String createIdxString = "CREATE " + isUniqueStr + " INDEX " +
-        idxName + " ON " + this.tableName + " (" + colNamesStr + ")";
-    this.executeStatement(createIdxString);
-  }
-
-  public void dropIndex(final String idxName) throws SQLException {
-    final String dropIdxString = "ALTER TABLE " + this.tableName + " DROP INDEX " + idxName;
-    this.executeStatement(dropIdxString);
-  }
-
-  private void executeStatement(final String stmtString)
-      throws SQLException {
-    try (final Statement stmt = getConn().createStatement()) {
-      stmt.execute(stmtString);
-      System.out.println("Successfully altered table " +
-          this.tableName);
-    }
-  }
 
   public void insert(final Object... values) throws SQLException {
     final String[] stringValues = new String[values.length];
@@ -149,9 +102,6 @@ public class Table {
   private void createNewRow(final String[] values, final ResultSet rs)
       throws SQLException {
     final List<String> insertColNames = this.getInsertCols();
-    for (final String insertCol : insertColNames) {
-      System.out.println(insertCol);
-    }
     for (int i = 0; i < insertColNames.size(); i++) {
       rs.updateObject(insertColNames.get(i), values[i]);
     }
@@ -159,155 +109,67 @@ public class Table {
 
   private List<String> getInsertCols() {
     final List<String> insertColNames = new ArrayList<>(this.colsCount);
-    for (int i = 0; i < this.colsCount; i++) {
-      if (!this.autoIncColIndices.contains(i + 1)) {
-        insertColNames.add(this.colNames.get(i));
+    for (final Column column : this.columnsList) {
+      if (column.isAutoIncrement()) {
+        insertColNames.add(column.getName());
       }
     }
     return insertColNames;
   }
 
-  private String createColumnsStr() {
-    final List<String> colDeclarationsList = new ArrayList<>(this.colsCount);
-    for (int i = 0; i < this.colsCount; i++) {
-      colDeclarationsList.add(this.getColStr(i));
-    }
-    return String.join(LINE_SEPARATOR, colDeclarationsList);
-  }
-
-  private String getColStr(final int idx) {
-    return this.colNames.get(idx) + " " + this.colTypes.get(idx)
-        + this.getNotNullStr(idx + 1) + this.getAutoIncStr(idx + 1)
-        + this.getDefaultValueStr(idx + 1);
-  }
-
-  private String getNotNullStr(final int idx) {
-    final boolean isNotNull = this.notNullColIndices.contains(idx);
-    return isNotNull ? " NOT NULL" : "";
-  }
-
-  private String getAutoIncStr(final int idx) {
-    final boolean isAutoInc = this.autoIncColIndices.contains(idx);
-    return isAutoInc ? " AUTO_INCREMENT" : "";
-  }
-
-  private String getDefaultValueStr(final int colIdx) {
-    final String defaultValue = this.defaultValues.get(colIdx);
-    return defaultValue == null ? "" : " DEFAULT " + defaultValue;
-  }
-
-  private String getPrimaryKeyStr() {
-    if (this.primaryKeyColName == null) return "";
-    return LINE_SEPARATOR + "PRIMARY KEY (" + this.primaryKeyColName + ")";
-  }
-
-  public Table setPrimaryKeyField(final String... colNames) {
-    this.primaryKeyColName = String.join(", ", colNames);
-    return this;
-  }
-
-  public Table setPrimaryKeyField(final int colIdx) {
-    if (colIdx <= this.colsCount) {
-      this.primaryKeyColName = this.colNames.get(colIdx - 1);
-    }
-    return this;
-  }
-
-  public Table setForeignKey(final String colName, final String foreignTable,
-                             final String foreignColName,
-                             final boolean isCascadeDelete) {
-    if (this.colNames.contains(colName)) {
-      final String cascadeDeleteStr = isCascadeDelete ? " ON DELETE CASCADE" :
-          "";
-      final String constraintStr = "FOREIGN KEY (" + colName + ") REFERENCES " +
-          foreignTable + "(" + foreignColName + ")" + cascadeDeleteStr;
-      this.foreignKeyConstraints.add(constraintStr);
-    }
-    return this;
-  }
-
-  public Table setForeignKey(final int colIdx, final String foreignTable,
-                             final String foreignColName) {
-    if (colIdx <= this.colsCount) {
-      final String constraintStr = "FOREIGN KEY (" + this.colNames.get(colIdx - 1) +
-          ") REFERENCES " + foreignTable + "(" + foreignColName + ")";
-      this.foreignKeyConstraints.add(constraintStr);
-    }
-    return this;
+  private String getColumnsStr() {
+    final List<String> colStrList = this.columnsList.stream().
+        map(Object::toString).toList();
+    return String.join(LINE_SEPARATOR, colStrList);
   }
 
   public Table setNotNullColumns() {
-    for (int i = 0; i < this.colsCount; i++) {
-      this.notNullColIndices.add(i + 1);
+    for (final Column column : this.columnsList) {
+      column.setNotNull();
     }
     return this;
   }
 
-  public Table setNotNullColumns(final Integer... cols) {
-    this.notNullColIndices = Arrays.asList(cols);
-    return this;
-  }
-
-  public Table setNotNullColumns(final String... colNames) {
-    for (final String colName : colNames) {
-      final int idx = this.colNames.indexOf(colName);
-      if (!(idx == -1 || this.notNullColIndices.contains(idx + 1))) {
-        this.notNullColIndices.add(idx + 1);
-      }
+  public Table column(final String colName) {
+    for (final Column column : this.columnsList) {
+      if (column.getName().equals(colName)) this.currColumn = column;
     }
     return this;
   }
 
-  public Table setDefaultValue(final int colIdx, final String defaultVal) {
-    this.defaultValues.put(colIdx, defaultVal);
+  public Table primaryKey() {
+    this.currColumn.setPrimaryKey();
     return this;
   }
 
-  public Table setDefaultValue(final String colName, final String defaultVal) {
-    final int idx = this.colNames.indexOf(colName);
-    if (idx != -1) {
-      this.defaultValues.put(idx + 1, defaultVal);
-    }
+  public Table notNull() {
+    this.currColumn.setNotNull();
     return this;
   }
 
-  public Table setAutoIncCol(final int colIdx) {
-    if (colIdx <= this.colsCount && !this.autoIncColIndices.contains(colIdx)) {
-      this.autoIncColIndices.add(colIdx);
-    }
+  public Table defaultValue(final String defaultValue) {
+    this.currColumn.setDefaultValue(defaultValue);
     return this;
   }
 
-  public Table setAutoIncCol(final String colName) {
-    final int idx = this.colNames.indexOf(colName);
-    if (idx != -1 && !this.autoIncColIndices.contains(idx + 1)) {
-      this.autoIncColIndices.add(idx + 1);
-    }
+  public Table reference(final String tableName, final String colName,
+                         final boolean isCascadeDelete) {
+    this.currColumn.setReferenceCol(tableName, colName, isCascadeDelete);
     return this;
   }
 
-  public Table setUniqueCol(final int colIdx) {
-    if (colIdx <= this.colsCount) {
-      this.uniqueColStatements.add("UNIQUE (" + this.colNames.get(colIdx - 1) +
-          ")");
-    }
+  public Table unique() {
+    this.currColumn.setUnique();
     return this;
   }
 
-  public Table setUniqueCol(final String colName) {
-    if (this.colNames.contains(colName) &&
-        !this.uniqueColStatements.contains(colName)) {
-      this.uniqueColStatements.add("UNIQUE (" + colName + ")");
-    }
+  public Table autoIncrement() {
+    this.currColumn.setAutoIncrement();
     return this;
   }
 
-  public Table setCheck(final String colName, final String operator,
-                        final String value) {
-    final String checkStr = "CHECK (" + colName + operator + value + ")";
-    if (!this.checkStatements.contains(checkStr)) {
-      this.checkStatements.add(checkStr);
-    }
+  public Table check(final String operator, final String value) {
+    this.currColumn.setCheck(operator, value);
     return this;
   }
 
